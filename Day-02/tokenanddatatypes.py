d26372fe94306b9c7db94d9fbb0b7308a31fb76b
Syed Mohammad Asif Amaan <139234447+Asif-17@users.noreply.github.com>
2026-04-15 23:33:41 +0530

    Add files via upload

---
+++ Day-02/tokenanddatatypes.py
@@ -0,0 +1,190 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:\Users\Thummala Sudhakar\Desktop\Python full stack course work\Day-1\Student_info.py
+Even
+>>> 
+= RESTART: C:\Users\Thummala Sudhakar\Desktop\Python full stack course work\Day-1\Student_info.py
+>>> myvar = 10
+>>> myvar
+10
+>>> MYVAR=10
+>>> myVar=10
+myvar1=10
+my_var=10
+my var=10
+SyntaxError: invalid syntax
+my@var=10
+SyntaxError: cannot assign to expression here. Maybe you meant '==' instead of '='?
+my(var)=10
+SyntaxError: cannot assign to function call here. Maybe you meant '==' instead of '='?
+_myvar=10
+1myvar=10
+SyntaxError: invalid decimal literal
+if =10
+SyntaxError: invalid syntax
+else =10
+SyntaxError: invalid syntax
+for =10
+SyntaxError: invalid syntax
+Myvar=10
+myvar=20
+Myvar
+10
+myvar
+20
+a=10
+A=10
+a
+10
+b
+Traceback (most recent call last):
+  File "<pyshell#21>", line 1, in <module>
+    b
+NameError: name 'b' is not defined
+A
+10
+
+= RESTART: C:\Users\Thummala Sudhakar\Desktop\Python full stack course work\Day-1\Student_info.py
+['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+35
+a=10
+a=20
+a
+20
+a=b=c=10
+a
+10
+b
+10
+c
+10
+a,b,c=10,20,30
+a
+10
+b
+20
+c
+30
+a=
+SyntaxError: incomplete input
+a
+10
+b
+20
+a=b
+a
+20
+b
+20
+b=a
+a
+20
+a
+20
+a=10
+b
+20
+a,b=b,a
+a
+20
+b
+10
+a=1243
+a
+1243
+type(a)
+<class 'int'>
+b=23.4
+b
+23.4
+type(b)
+<class 'float'>
+c= 12+j
+Traceback (most recent call last):
+  File "<pyshell#54>", line 1, in <module>
+    c= 12+j
+NameError: name 'j' is not defined
+c=12+3j
+c
+(12+3j)
+s='pyython programming'
+type(s)
+<class 'str'>
+s="hello"\
+
+
+   
+
+
+s='''sdfghj'''
+s="'fghjkl'"
+l=[1,2,3,4,5,6]
+l
+[1, 2, 3, 4, 5, 6]
+l=[1.2,3,'gfds,',[1,2,3]]
+l
+[1.2, 3, 'gfds,', [1, 2, 3]]
+a=10
+a=20
+a
+20
+id(a)
+140722968323464
+a=30
+a
+30
+id(a)
+140722968323784
+l=[1,2,3,4]\
+
+
+   
+
+l
+[1, 2, 3, 4]
+l.append(5)
+l
+[1, 2, 3, 4, 5]
+id(l)
+2711083382976
+l.append(89)
+l
+[1, 2, 3, 4, 5, 89]
+id(l)
+2711083382976
+l=[1,2,3]
+l
+[1, 2, 3]
+l
+[1, 2, 3]
+l
+[1, 2, 3]
+t=(12.34567,12.56789)
+l=[]
+t=()
+type(l)
+<class 'list'>
+type(t)
+<class 'tuple'>
+s={1,546789,9865,1321,3432,5768}
+s
+{1, 9865, 546789, 3432, 1321, 5768}
+s.add(1)
+s
+{1, 9865, 546789, 3432, 1321, 5768}
+s={1,1,1,1,1,3,4,2,2,2,2,2}
+s
+{1, 2, 3, 4}
+data ={'name':'nagaraju',}
+data ={'name':'nagaraju','course':'PFS','skills':['python','java']}
+data
+{'name': 'nagaraju', 'course': 'PFS', 'skills': ['python', 'java']}
+status = False
+True
+True
+type(status)
+<class 'bool'>
+a=None
+type(a)
+<class 'NoneType'>
